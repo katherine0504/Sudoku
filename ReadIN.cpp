a11705ec03fb57d9b75eb2635a024d13254e1bc2
katherine0504 <F74036069@ncku.edu.tw>
2015-04-16 13:26:39 +0800

    0416_1330

--- ReadIN.cpp
+++ ReadIN.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
 #include <cstdlib>
+#include <iomanip>
 
-#define N 12
+#define N 9
 #define SQN 2
+
+using namespace std;
 
 int main()
 {
    int col;
    int row;
+   int grid[N][N];
 
-   for (col=0; col<9; col++)
-	  for (row=0; row<9; row++)
+   for (col=0; col<N; col++)
+	  for (row=0; row<N; row++)
+		 cin >> grid[col][row];
+
+   for (col=0; col<N; col++)
+   {
+	  for (row=0; row<N; row++)
+		 cout << setw(3) << grid[col][row];
+	  
+	  cout << endl;
+   }
+
+
+   return 0;
+}
+
 
 
