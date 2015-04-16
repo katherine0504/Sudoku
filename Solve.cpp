22df6d99b6f1b94c90792fc60a398fd728c5827c
katherine0504 <F74036069@ncku.edu.tw>
2015-04-16 20:46:18 +0800

    0416_20450416_20450416_20450416_2045

--- Solve.cpp
+++ Solve.cpp
@@ -14,15 +14,34 @@ bool FindUnassignedLocation(int grid [N][N], int &row, int &col);
 
 bool Safe(int grid[N][N], int row, int col, int num);
 
+int CopyGrid(int grid[N][N])
+{
+   int row;
+   int col;
+   int gridCopy[N][N];
+
+   for (row=0; row<N; row++)
+   {
+	  for (col=0; col<N; col++)
+	  {
+		 gridCopy[row][col]=grid[row][col];
+		 cout << gridCopy[row][col];
+	  }
+
+	  cout << endl;
+   }
+}
+
 bool SolveSudoku (int grid[N][N])
 {
    int row;
    int col;
+   int gridCheck[N][N];
 
    if (!FindUnassignedLocation(grid, row, col))
 	  return true;
 
-   for (int num=9; num>=0; num--)
+   for (int num=1; num<=9; num++)
    {
 	  if (Safe (grid, row, col, num))
 	  {
@@ -81,7 +100,7 @@ bool Safe (int grid[N][N], int row, int col, int num)
    return !UsedInRow(grid, row, col, num) && !UsedInCol(grid, row, col, num) && !UsedInBox(grid, row-(row%SQN), col-(col%SQN), num);
 }
 
-void printGrid (int grid[N][N])
+void PrintGrid (int grid[N][N])
 {
    for (int row=0; row<N; row++)
    {
@@ -95,21 +114,23 @@ void printGrid (int grid[N][N])
 int main()
 {
    int grid[N][N]={
-	  {9,8,5,3,6,1,2,7,4},
-	  {7,0,0,8,2,9,3,5,6},
-	  {6,3,2,4,5,7,1,9,8},
-	  {8,0,0,9,3,6,7,2,5},
-	  {3,5,9,2,7,8,6,4,1},
-	  {2,7,6,5,1,4,9,8,3},
-	  {5,2,7,6,4,3,8,1,9},
-	  {4,9,3,1,8,2,5,6,7},
-	  {1,6,8,7,9,5,4,3,2}
+	  {9,8,5,3,6,0,0,7,4},
+	  {7,0,0,8,0,9,3,5,6},
+	  {6,3,0,4,5,7,0,9,8},
+	  {8,0,0,9,3,6,7,0,5},
+	  {3,5,9,0,7,8,6,4,0},
+	  {0,7,6,5,0,4,9,8,3},
+	  {5,0,7,6,4,3,8,0,9},
+	  {4,9,3,0,8,0,5,6,7},
+	  {0,6,8,7,9,5,4,3,0}
    };
+
+   int CopyGrid(grid[N][N]);
 
    if (SolveSudoku(grid) == true)
    {
 	  cout << "1" << endl;
-	  printGrid(grid);
+	  PrintGrid(grid);
    }
 
    else
